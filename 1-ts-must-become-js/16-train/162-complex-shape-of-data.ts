de46f35b673d19d08e6b9934126948645e6ba809
Jabi <michal.jabi@gmail.com>
2022-11-17 13:59:33 +0100

    feat(162): make it work.

--- 1-ts-must-become-js/16-train/162-complex-shape-of-data.ts
+++ 1-ts-must-become-js/16-train/162-complex-shape-of-data.ts
@@ -10,15 +10,23 @@
  * next:
  * - create the appropriate objects and pass them to `giveFullName` to confirm that the code works correctly
  * */
+ 
 
-function joiner (strTuple: any): any  {
+function joiner (strTuple: [string, string]): string  {
     return strTuple[0] + ' ' + strTuple[1];
 }
 
-function giveFullName(myUser: any): any {
-    const fullName: any = myUser.name + ' ' + myUser.lastName;
-    let prefix: any;
-    let suffix: any;
+interface User {
+    name: string;
+    lastName: string;
+    sex: string;
+    vipStatus: boolean;
+}
+
+function giveFullName(myUser: User): string {
+    const fullName: string = myUser.name + ' ' + myUser.lastName;
+    let prefix = '';
+    let suffix = '';
     if(myUser.sex === 'female') {
         prefix = 'Mrs.';
     } else if(myUser.sex === 'male') {
