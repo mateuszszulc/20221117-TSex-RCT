0eb356b363cab043c5a51ecfc5eedc0eca2a6c88
Jabi <michal.jabi@gmail.com>
2022-11-17 12:57:27 +0100

    explain(121): object + strict: true

--- 1-ts-must-become-js/12-complex-types/121-objects.ts
+++ 1-ts-must-become-js/12-complex-types/121-objects.ts
@@ -6,18 +6,26 @@
  * First a simple example....
  */
 
+type NamedObject = { name?: string }
+
 // When making a classic object in JS, all we need is the following:
-const myObject = {};
+const myObject: NamedObject = { };
 
 // Now, we expect that you can dynamically extend the object with new fields and values
 // property: value
+
+console.log(myObject.name)
+
+myObject.name?.toUpperCase();
 
 // In classic JS, just add a new field and assign it a value
 // However, it turns out that this is not possible in TypeScript:
 
 // Uncomment to check:
-// myObject.name = 'toolbox';
+myObject.name = 'toolbox';
 
+
+myObject.name.toUpperCase();
 /*
 
 This is done through "type inference"
