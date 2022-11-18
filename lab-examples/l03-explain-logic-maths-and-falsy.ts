ab40b582d9b7f495139d41cfd32b67d7eb6cddbd
Jabi <michal.jabi@gmail.com>
2022-11-18 13:05:12 +0100

    explain(l03): falsy and math logic

---
+++ lab-examples/l03-explain-logic-maths-and-falsy.ts
@@ -0,0 +1,16 @@
+console.log(0 && 'Hello');
+
+console.log(1 && 'Hello' && false && '!');
+
+
+// Falsy values:
+// 
+Boolean(NaN); //?
+Boolean(0); //?
+
+
+if ({}) {
+    console.log('!')
+}
+
+Boolean({}); //?
